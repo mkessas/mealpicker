512ea00e6b75007f8d4b8de389797259171be1c5
Mourad <mourad@kessas.com>
2016-06-12 15:07:50 +1200

    Tag selection working

--- js/mealpicker.js
+++ js/mealpicker.js
@@ -1,19 +1,47 @@
+/* Hello, this is Dinner Time */
+
 app = angular.module('mealpickerApp', []);
 
 app.controller('mealCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.tags = [ ];
     $scope.recipes = {};
+    $scope.selectedTags = {};
+    $scope.selectedRecipes = {};
 
-    $scope.getRecipes = function() {
+    $scope.toggleTag = function(tag) {
+        $scope.selectedRecipes = {};
+        for (var recipe in $scope.recipes) {
+            var tagCount = 0;
+            var tagMatchCount = 0;
+            for (var tag in $scope.selectedTags) {
+                if ($scope.selectedTags[tag] == false) continue;
+                tagCount++;
+                if ($scope.recipes[recipe].indexOf(tag) > -1) tagMatchCount++;
+            }
+            if (tagMatchCount == tagCount) $scope.selectedRecipes[recipe] = $scope.recipes[recipe];
+        }
+    }
+
+    $scope.count = function(tag) {
+        var count = 0;
+        angular.forEach($scope.recipes, function(r) {
+            for (var t in r) {
+                if (r[t] == tag) count++;
+            }
+        });
+        return count;
+    }
+
+    $scope.loadRecipes = function() {
         $http.get("list.json").then(function (response) {
             var tags = {};
             $scope.recipes = response.data;
+            $scope.selectedRecipes = response.data;
             for (recipe in response.data) {
                 for (tag in response.data[recipe]) {
                     tags[response.data[recipe][tag]] = 1;
                 }
             }
-            console.log(tags);
             $scope.tags = Object.keys(tags).sort();
         },
         function(response) {
@@ -21,14 +49,28 @@ app.controller('mealCtrl', ['$scope', '$http', function ($scope, $http) {
         });
     }
 
-    $scope.getRecipes();
+    $scope.tagCount = function() {
+        var count = 0;
+        angular.forEach($scope.selectedTags, function (t,v) {
+            if ($scope.selectedTags[v] == true) count++;
+        });
+        return count;
+    }
+
+    $scope.recipeCount = function() {
+        return $scope.selectedRecipes.length;
+    }
+
+
+    $scope.loadRecipes();
 
 }]);
+
 
 app.filter('searchFilter', function() {
     return function(input, search) {
         if (!search) return input;
-    if (!input) return input;
+        if (!input) return input;
         var expected = ('' + search).toLowerCase();
         var result = {};
         angular.forEach(input, function(value, key) {
